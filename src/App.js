6d733e4cbf673fa07020dd1ddbe5aa0836208742
Tony Monda <ajmonda@gmail.com>
2020-12-05 16:03:58 -0500

    services functions for api call and sorting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-import queryString from 'query-string'
+import "./App.css";
+import { getTopArtists, getPercentages, groupArtists } from "./services";
+
+import queryString from "query-string";
 
 function App() {
+  const [artists, setArtists] = useState([]);
+  const string = queryString.parse(window.location.search);
+  const accessToken = string.access_token;
 
-  const string = queryString.parse(window.location.search)
-  const accessToken = string.access_token
+  useEffect(() => {
+    const apiCall = async () => {
+      const topArtists = await getTopArtists(accessToken);
+      setArtists(topArtists);
+    };
+    apiCall();
+  }, []);
+
+  // determine "uniqueness" avg of artists
+
+  // group artists by popularity in quarter increments
+  const ranking = groupArtists(artists);
+
+  // // assign percentage to each grouping
+  console.log(getPercentages(ranking));
+
+  // // generate dichromatic scale based on percentages
+
+  // // hover over grouping displays artists in group
+
+  // // // deep cuts playlist
+
 
   return accessToken ? (
-
     <div className="App">
-      
       <h1>Hello</h1>
     </div>
-  )
-    : (
-      // Login component here
+  ) : (
+    // Login component here
     <button>Login</button>
   );
 }
